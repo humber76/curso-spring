078d601e7099d962be2aee5a8090306f15eefcfb
Humberto Hernandez <hdezhh@gmail.com>
2014-02-26 16:11:55 -0600

    guardar  constatne de prueba

--- src/main/java/org/upiita/spring/jdbc/daos/Constantes.java
+++ src/main/java/org/upiita/spring/jdbc/daos/Constantes.java
@@ -4,6 +4,11 @@ import org.upiita.spring.jdbc.entidades.Usuario;
 
 public class Constantes {
 	
+	
+	//Esto es una constante que  no marca
+	
+	public static String ENTIDAD_USUARIO_Apellido = "apellido";
+	
 	public static String ENTIDAD_USUARIO_NOMBRE = "nombre";
 
 
